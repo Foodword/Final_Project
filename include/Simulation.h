d9d0636e928b66ac860feb73f1f298e185950e3a
Foodword <saahirba07@gmail.com>
2025-10-20 11:37:38 -0700

    Update Simulation.h

--- include/Simulation.h
+++ include/Simulation.h
@@ -1,1 +1,16 @@
+#ifndef CONWAY_S_GAME_OF_LIFE_SIMULATION_H
+#define CONWAY_S_GAME_OF_LIFE_SIMULATION_H
+#include "Map.h"
 
+class Simulation {
+    private:
+    Map map;
+
+    public:
+    Simulation(Map map);
+    ~Simulation();
+    void run(); // runs a simulation and asks the user for an input to change a cell from alive to dead
+};
+
+
+#endif
