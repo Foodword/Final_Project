f76fd23f684f84bc3727dad6ef47e1e91968acfe
Foodword <saahirba07@gmail.com>
2025-10-20 11:35:43 -0700

    Update Map.h

--- include/Map.h
+++ include/Map.h
@@ -1,1 +1,18 @@
+#ifndef CONWAY_S_GAME_OF_LIFE_MAP_H
+#define CONWAY_S_GAME_OF_LIFE_MAP_H
 
+
+class Map {
+private:
+int width;
+int height;
+Cell **map;
+
+public:
+    Map(int w, int h);
+    ~Map();
+
+};
+
+
+#endif
